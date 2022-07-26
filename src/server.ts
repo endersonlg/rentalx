1a6336f48e1b33c9378eecba162ef3cae0e06f78
endersonlg <endersonluciog@gmail.com>
2022-07-26 17:02:13 -0300

    added config to debug

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,12 @@ app.get("/", (request, response) => {
   return response.json({ message: "Hello world" });
 });
 
+app.use(express.json());
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  response.json({ name });
+});
+
 app.listen(3333, () => console.log("Server is running!"));
